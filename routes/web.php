484f8be44c4058a3af57338788b09d0ee5a49e42
henry <henrysaja1102@gmail.com>
2026-05-02 20:37:55 +0700

    belajar link

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/fakultas', function () {
+    return view('fakultas.list-fakultas');
+});
+route::get('/fakultas/edit', function () {
+    return view('fakultas.edit-fakultas');
+});
+
